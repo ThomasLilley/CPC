a26798c37bed6e83d922c35f90c8b7c35f1b7646
ThomasLilley <thomas.lilley01@gmail.com>
2018-05-04 01:29:47 +0100

    fixed plugin issue caused by whitelist

--- www/js/main.js
+++ www/js/main.js
@@ -9,6 +9,8 @@ function onDeviceReady() {
     document.getElementById("cameraTakeProfPicture").addEventListener
     ("click", cameraTakeProfPicture);
 
+    document.getElementById("star1")
+
 	init();
 }
 
@@ -86,3 +88,33 @@ $(document).on('pagecreate', '#menu', function () {
 	console.log("pagecreate menu");
 
 });
+
+var gStorage = {};
+
+function toogle(anImage, anAltSrcArr) {
+    if (typeof(anImage) === "undefined" || typeof(anAltSrcArr) === "undefined" || anAltSrcArr.length === 0) {
+        return;
+    }
+
+    var id = anImage.id;
+    var oldSrc = anImage.src;
+
+    if (typeof(gStorage[id]) === "undefined") {
+        gStorage[id] = {
+            'id': id,
+            'origSrc': oldSrc,
+            'i': 0
+        };
+    }
+
+    gStorage[id].i += 1;
+    if (gStorage[id].i > anAltSrcArr.length) {
+        gStorage[id].i = 0;
+    }
+
+    if (gStorage[id].i === 0) {
+        anImage.src = gStorage[id].origSrc;
+    } else {
+        anImage.src = anAltSrcArr[gStorage[id].i - 1];
+    }
+}
